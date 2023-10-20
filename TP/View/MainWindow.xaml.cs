6c1c5c7131ae9094f0916425fa79a0a269d462d9
GTai <43651069+TalosGTai@users.noreply.github.com>
2023-10-20 08:19:07 +0300

    Добавьте файлы проекта.

---
+++ TP/View/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int idOrg;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(int idOrg)
+        {
+            InitializeComponent();
+            this.idOrg = idOrg;
+            Title += idOrg.ToString();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            // create_window
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ChoiceLab_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            Close();
+            authWindow.Show();
+        }
+    }
+}
