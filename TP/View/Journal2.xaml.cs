f8516826a0e8051dbcaf89acb383f89cd18e9f7f
GTai <43651069+TalosGTai@users.noreply.github.com>
2023-10-27 19:53:00 +0300

    Add
    • SlideMenu model

---
+++ TP/View/Journal2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Journal2.xaml
+    /// </summary>
+    public partial class Journal2 : Page
+    {
+        public Journal2()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnJournal1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in App.Current.Windows)
+            {
+                if (window.Name == "TPWindow")
+                {
+                    MainWindow current = window as MainWindow;
+                    current.ViewPages.Content = new Journal1();
+                }
+            }
+        }
+    }
+}
