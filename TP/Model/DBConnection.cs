c97abcd5ad5c2513389aa2432999d92c229d76b6
Kaija <rl-c@yandex.ru>
2023-12-21 19:00:06 +0300

    ADD -include dbconnection into project -include config.json into project -nuget package system.text.json

--- TP/Model/DBConnection.cs
+++ TP/Model/DBConnection.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace TP.Model
 {
